7e320efa50cfbbc51eeeee503efef15d8c7fc6ee
blogscot <iain.diamond@gmail.com>
2015-07-29 19:16:00 +0100

    Update Strain

--- strain/Strain.hs
+++ strain/Strain.hs
@@ -1,7 +1,15 @@
 module Strain (keep, discard) where
 
-keep :: (a -> Bool) -> [a] -> [a]
-keep p lst = [ x | x <- lst, p x]
+keep'' :: (a -> Bool) -> [a] -> [a]
+keep'' p lst = [ x | x <- lst, p x]
 
 discard :: (a -> Bool) -> [a] -> [a]
 discard p = keep (not . p)
+
+keep' :: (a -> Bool) -> [a] -> [a]
+keep' _ [] = []
+keep' p (x:xs) | p x = x : keep' p xs
+               | otherwise = keep' p xs
+
+keep :: (a->Bool) -> [a] -> [a]
+keep p = foldr (\x y -> if p x then x:y else y) []
