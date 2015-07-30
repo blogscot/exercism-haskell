093a387dc14042f42d33093934ac1e4cd6de6837
blogscot <iain.diamond@gmail.com>
2015-07-30 18:54:50 +0100

    Add nucleotide-count solution

---
+++ nucleotide-count/DNA.hs
@@ -0,0 +1,18 @@
+module DNA (count, nucleotideCounts) where
+
+import Data.Map.Strict (Map, fromList)
+--
+dna :: String
+dna = "ACGT"
+
+validate :: Char -> Char
+validate c
+  | c `notElem` dna = error $ "invalid nucleotide " ++ show c
+  | otherwise = c
+
+count :: Char -> String -> Int
+count c str = length $ filter matches $ map validate str
+  where matches x = x == validate c
+
+nucleotideCounts :: String -> Map Char Int
+nucleotideCounts str = fromList [(c, count c str) | c <- dna]
