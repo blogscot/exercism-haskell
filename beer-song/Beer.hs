5ffbc2bbbf35a2ffc38d10b89e3abf581cf5eead
blogscot <iain.diamond@gmail.com>
2015-08-10 20:05:10 +0100

    Add beer song solution

---
+++ beer-song/Beer.hs
@@ -0,0 +1,27 @@
+module Beer (sing, verse) where
+
+verse :: Int -> String
+verse n
+  | n > 0 = howManyBottles n " bottle" ++ " of beer on the wall, " ++
+            howManyBottles n " bottle" ++ " of beer.\n" ++
+            takeBottles n "Take one" ++ " down and pass it around, " ++
+            howManyBottles (pred n) " bottle" ++ " of beer on the wall.\n"
+  | otherwise = "No more bottles of beer on the wall, no more bottles of beer.\n" ++
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+takeBottles :: Int -> String -> String
+takeBottles n s
+  | n > 1 = s
+  | n == 1 = "Take it"
+  | otherwise = error "Negative number of bottles!"
+
+howManyBottles :: Int -> String -> String
+howManyBottles n s
+  | n > 1 = show n ++ s ++ "s"
+  | n == 1 = show n ++ s
+  | n == 0 = "no more bottles"
+  | otherwise = error "Negative number of bottles!"
+
+sing :: Int -> Int -> String
+sing start end = concatMap verse' [start, pred start..end]
+  where verse' x = verse x ++ "\n"
