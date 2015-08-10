39409a54de796d7e9e7b813953a9c4cf5fcf0b25
blogscot <iain.diamond@gmail.com>
2015-08-10 17:35:56 +0100

    Initial meetup solution

--- meetup/Meetup.hs
+++ meetup/Meetup.hs
@@ -1,4 +1,4 @@
-module Meetup (Weekday(..), Schedule) where
+module Meetup (Weekday(..), Schedule(..), meetupDay) where
 
 import Data.Time.Calendar
 
@@ -12,9 +12,14 @@ data Schedule = First | Second | Third | Fourth | Teenth | Last
   deriving (Enum, Show)
 
 meetupDay :: Schedule -> Weekday -> Year -> Month -> Day
-meetupDay = undefined
--- meetupDay sch wk y m
---   | First _ _ _ = weekdays y m
+meetupDay First wd y m = snd . head $ daysOfWeek wd y m
+meetupDay Second wd y m = snd . head . tail $ daysOfWeek wd y m
+meetupDay Third  wd y m = snd . head . tail . tail $ daysOfWeek wd y m
+meetupDay Fourth wd y m = snd . head . tail . tail . tail $ daysOfWeek wd y m
+meetupDay Last wd y m = snd . last $ daysOfWeek wd y m
+meetupDay Teenth wd y m = head [ date |(_, date) <- daysOfWeek wd y m, check $ show date]
+  where day date = read $ drop 8 date
+        check date = day date >= 13 && day date <= 19
 
 offset :: Integer -> Int -> Int -> Weekday
 offset year month day = toEnum offset'
@@ -26,14 +31,24 @@ weekdays :: Integer -> Int -> [(Weekday, Int)]
 weekdays year month = [(offset year month day, day) | day <- [1..monthLength]]
   where monthLength = gregorianMonthLength year month
 
-daysofweek :: Weekday -> Year -> Month -> [(Weekday, Int)]
-daysofweek wkdy y m = [ (weekday, day) | (weekday, day) <- weekdays (toInteger y) m, weekday == wkdy]
+daysOfWeek :: Weekday -> Year -> Month -> [(Weekday, Day)]
+daysOfWeek wd y m = [ (weekday, fromGregorian (toInteger y) m day) | (weekday, day)
+                      <- weekdays (toInteger y) m, weekday == wd]
 
 
-main = do
+-- main = do
   -- print $ fromEnum Teenth
   -- print $ ModifiedJulianDay 57245
   -- print $ toGregorian $ fromGregorian 2011 12 24
   -- print $ weekdays 2015 08
   -- print [ (weekday, day) | (weekday, day) <- weekdays 2015 08, weekday == Saturday]
-  print $ daysofweek Saturday 2015 08
+  -- print $ daysOfWeek Saturday 2015 08
+  -- print $ snd . head $ daysOfWeek Saturday 2015 08
+  -- print $ snd . head . tail $ daysOfWeek Saturday 2015 08
+  -- print $ snd . last $ daysOfWeek Saturday 2015 08
+  -- print [ date |(_, date) <- daysOfWeek Saturday 1967 08, check $ show date]
+  --   where day date = read $ drop 8 date
+  --         check date  = day date >= 13 && day date <= 19
+  -- print $ meetupDay First Monday 2015 08
+  -- print $ meetupDay Last Monday 2015 08
+  -- print $ meetupDay Teenth Monday 2015 08
