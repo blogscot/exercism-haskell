362a56d23febe9f420558a03f5f53ba20f3c3585
blogscot <iain.diamond@gmail.com>
2015-08-10 17:56:59 +0100

    meetup.hs, minor refactoring

--- meetup/Meetup.hs
+++ meetup/Meetup.hs
@@ -13,13 +13,13 @@ data Schedule = First | Second | Third | Fourth | Teenth | Last
 
 meetupDay :: Schedule -> Weekday -> Year -> Month -> Day
 meetupDay First wd y m = snd . head $ daysOfWeek wd y m
-meetupDay Second wd y m = snd . head . tail $ daysOfWeek wd y m
-meetupDay Third  wd y m = snd . head . tail . tail $ daysOfWeek wd y m
-meetupDay Fourth wd y m = snd . head . tail . tail . tail $ daysOfWeek wd y m
+meetupDay Second wd y m = snd (daysOfWeek wd y m !! 1)
+meetupDay Third  wd y m = snd (daysOfWeek wd y m !! 2)
+meetupDay Fourth wd y m = snd (daysOfWeek wd y m !! 3)
 meetupDay Last wd y m = snd . last $ daysOfWeek wd y m
-meetupDay Teenth wd y m = head [ date |(_, date) <- daysOfWeek wd y m, check $ show date]
-  where day date = read $ drop 8 date
-        check date = day date >= 13 && day date <= 19
+meetupDay Teenth wd y m = head [ date |(_, date) <- daysOfWeek wd y m, check date]
+  where day date = fromInteger $ read $ drop 8 $ show date
+        check date = (day date :: Int) >= 13 && (day date :: Int) <= 19
 
 offset :: Integer -> Int -> Int -> Weekday
 offset year month day = toEnum offset'
@@ -35,8 +35,8 @@ daysOfWeek :: Weekday -> Year -> Month -> [(Weekday, Day)]
 daysOfWeek wd y m = [ (weekday, fromGregorian (toInteger y) m day) | (weekday, day)
                       <- weekdays (toInteger y) m, weekday == wd]
 
-
--- main = do
+main :: IO ()
+main = do
   -- print $ fromEnum Teenth
   -- print $ ModifiedJulianDay 57245
   -- print $ toGregorian $ fromGregorian 2011 12 24
@@ -49,6 +49,6 @@ daysOfWeek wd y m = [ (weekday, fromGregorian (toInteger y) m day) | (weekday, d
   -- print [ date |(_, date) <- daysOfWeek Saturday 1967 08, check $ show date]
   --   where day date = read $ drop 8 date
   --         check date  = day date >= 13 && day date <= 19
-  -- print $ meetupDay First Monday 2015 08
-  -- print $ meetupDay Last Monday 2015 08
-  -- print $ meetupDay Teenth Monday 2015 08
+  print $ meetupDay First Monday 2015 08
+  print $ meetupDay Last Monday 2015 08
+  print $ meetupDay Teenth Monday 2015 08
