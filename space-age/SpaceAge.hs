2965090585ca5a7ee4669359264746db29c5bb96
blogscot <iain.diamond@gmail.com>
2015-08-10 11:08:30 +0100

    Updated space-age solution

--- space-age/SpaceAge.hs
+++ space-age/SpaceAge.hs
@@ -12,12 +12,15 @@ data Planet = Mercury
 earthYear :: Double
 earthYear = 365.25 * 24 * 60 * 60
 
+ageOn' :: Double -> Double -> Double
+ageOn' secs ratio = secs / ratio / earthYear
+
 ageOn :: Planet -> Double -> Double
-ageOn Mercury x = x / earthYear / 0.2408467
-ageOn Venus x   = x / earthYear / 0.61519726
-ageOn Earth x   = x / earthYear
-ageOn Mars x    = x / earthYear / 1.8808158
-ageOn Jupiter x = x / earthYear / 11.862615
-ageOn Saturn x  = x / earthYear / 29.447498
-ageOn Uranus x  = x / earthYear / 84.016846
-ageOn Neptune x = x / earthYear / 164.79132
+ageOn Earth secs   = secs / earthYear
+ageOn Mercury secs = ageOn' secs 0.2408467
+ageOn Venus secs   = ageOn' secs 0.61519726
+ageOn Mars secs    = ageOn' secs 1.8808158
+ageOn Jupiter secs = ageOn' secs 11.862615
+ageOn Saturn secs  = ageOn' secs 29.447498
+ageOn Uranus secs  = ageOn' secs 84.016846
+ageOn Neptune secs = ageOn' secs 164.79132
