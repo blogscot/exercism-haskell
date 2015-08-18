e9c7b6ccf9db8f7dc20b60afd7c5ae2016e41b10
blogscot <iain.diamond@gmail.com>
2015-08-18 20:04:18 +0100

    Refactored raindrops solution

--- raindrops/Raindrops.hs
+++ raindrops/Raindrops.hs
@@ -1,20 +1,17 @@
 module Raindrops (convert) where
 
-import Data.Set as S hiding (map)
+import Data.Set hiding (map)
 import PrimeFactors
 
 convert :: Int -> String
 convert num
-  | any (\x -> x==3 || x==5 || x==7) factors = convert' factors
+  | any (\x -> x==3 || x==5 || x==7) factors = f factors
   | otherwise = show num
-    where factors = primeFactors num
-
-convert' :: [Int] -> String
-convert' nums = f nums'
-  where nums' = S.toList $ S.fromList nums
-        f (n:ns)
-          | n==3 = "Pling" ++ convert' ns
-          | n==5 = "Plang" ++ convert' ns
-          | n==7 = "Plong"
-          | otherwise = convert' ns
-        f _ = []
+    where factors = toList . fromList $ primeFactors num
+          f :: [Int] -> String
+          f [] = []
+          f (n:ns)
+            | n==3 = "Pling" ++ f ns
+            | n==5 = "Plang" ++ f ns
+            | n==7 = "Plong"
+            | otherwise = f ns
