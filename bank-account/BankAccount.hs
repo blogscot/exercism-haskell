e0eedf61a10df8a027b8e36cc1ff740bb950b059
blogscot <iain.diamond@gmail.com>
2015-08-23 19:04:11 +0100

    partial solution, bank account

--- bank-account/BankAccount.hs
+++ bank-account/BankAccount.hs
@@ -4,30 +4,33 @@ module BankAccount ( BankAccount
                    , getBalance
                    , incrementBalance) where
 
-
-import Control.Concurrent
+import Control.Concurrent.STM
 
 type Account = Int
 type Balance = Integer
-data BankAccount = BankAccount Account Balance
-  deriving (Eq, Show)
+data BankAccount = BankAccount Account (TVar Balance)
 
 openAccount :: IO BankAccount
-openAccount = return $ BankAccount 100 0
+openAccount = do
+  bal <- atomically $ newTVar 0
+  return $ BankAccount 100 bal
 
 closeAccount :: BankAccount -> IO ()
 closeAccount acc = return ()
 
 getBalance :: BankAccount -> IO (Maybe Integer)
-getBalance (BankAccount _ bal) = return $ Just bal
+getBalance (BankAccount acc bal) = atomically $ readTVar bal >>= return . Just
 
 incrementBalance :: BankAccount -> Integer -> IO (Maybe Integer)
-incrementBalance (BankAccount acc bal) amount = return $ Just (bal + amount)
-
+incrementBalance (BankAccount acc bal) amount = do
+  current <- atomically $ readTVar bal
+  let newBalance = current + amount
+  atomically $ writeTVar bal newBalance
+  return $ Just newBalance
 
 main = do
   acc <- openAccount
   getBalance acc >>= print
   bal <- incrementBalance acc 56
   print bal
-  -- getBalance acc >>= print
+  getBalance acc >>= print
