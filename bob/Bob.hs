4742a919e29131688508a39737441e5ca91c39b2
blogscot <iain.diamond@gmail.com>
2015-08-23 20:33:55 +0100

    Refactor Bob solution

--- bob/Bob.hs
+++ bob/Bob.hs
@@ -2,19 +2,12 @@ module Bob (responseFor) where
 
 import Data.Char
 
-isLast :: Eq a => a -> [a] -> Bool
-isLast _ [] = False
-isLast c str = last str == c
-
-hasLetters :: String -> Bool
-hasLetters = any isAlpha
-
-isUpperCase :: String -> Bool
-isUpperCase = all isUpper . filter isAlpha
-
 responseFor :: String -> String
 responseFor str
   | hasLetters str && isUpperCase str = "Whoa, chill out!"
-  | filter (not . isSpace) str == ""   = "Fine. Be that way!"
-  | '?' `isLast` str                   = "Sure."
-  | otherwise                          = "Whatever."
+  | all isSpace str                   = "Fine. Be that way!"
+  | last str == '?'                   = "Sure."
+  | otherwise                         = "Whatever."
+    where
+      hasLetters = any isAlpha
+      isUpperCase = all isUpper . filter isAlpha
